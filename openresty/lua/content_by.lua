cf2b5bab9daa6d9cd514efa33a0099a3ac85b078
liangdong <liangdong@smzdm.com>
2018-02-24 10:42:29 +0800

    update

--- openresty/lua/content_by.lua
+++ openresty/lua/content_by.lua
@@ -1,5 +1,6 @@
-local log = require("log")
-local json = require("cjson")
+local log = require("log") -- 自定义log库
+local json = require("cjson") -- json库
+local ck = require("cookie") -- cookie库
 
 -- 读取body
 local raw_msg = ngx.req.get_body_data()
@@ -8,16 +9,34 @@ if not raw_msg or string.len(raw_msg) == 0 then -- 上报内容为空, 直接返
     return
 end
 
+-- 解析cookie
+local cookie, err = ck:new()
+if not cookie then
+    ngx.log(ngx.ERR, err)
+    return
+end
+
+-- 获取cookie
+local all_cookies, err = cookie:get_all()
+if not all_cookies then
+    all_cookies = {}
+end
+-- 为cookie进行urldecode
+for cname, cvalue in pairs(all_cookies) do
+    all_cookies[cname] = ngx.unescape_uri(cvalue)
+end
+
 -- json数组解开为多条日志
 local raw_arr = json.decode(raw_msg)
 
--- 获取时间
-local timestamp = os.date("%Y%m%d%H", os.time())
-
+-- 重组日志
 local log_arr = {} -- 拆分后的json行数组
 for idx, log_item in pairs(raw_arr) do 
-    log_item.ip = ngx.var.remote_addr
-    log_item.timestamp = timestamp
+    -- 将cookie合并到日志行中
+    for cname, cvalue in pairs(all_cookies) do
+        log_item[cname] = cvalue
+    end
+    -- 序列化日志行, 放入待输出数组
     local log_item_str = json.encode(log_item)
     table.insert(log_arr, log_item_str)
 end
